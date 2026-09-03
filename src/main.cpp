7b67b289c6dbb9a94b6e1283d7c5c9cba801825f
Hbg1010 <harrisonbg1010@gmail.com>
2026-09-03 15:04:43 -0400

    End screen orb effects, refactor (for call order reasons)

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@ using namespace geode::prelude;
 
 // calculate the amount of orbs to display
 int orbCalc(int percent, int dif) {
+    // TODO: Impl fixes for main levels that have custom values (clamp SHOULD make it safe but still)
     const float orbVals[10] = {0.f, 50.f, 75.f, 125.f, 175.f, 225.f, 275.f, 350.f, 425.f, 500.f};
     dif = std::clamp(dif, 1, 10) - 1;
-    return (int) (percent == 100 
+    return (int) (percent == 100
             ? orbVals[dif] 
             : orbVals[dif] * .8f * (float) percent / 100.f);
 }
@@ -21,37 +22,39 @@ int diamondsCalc(int percent, int dif) {
     return (2 + dif) * percent / 100;
 }
 
-/*
-Artifical version of the CurrencyRewardLayer. Never directly adds anything
-*/
-class $modify(ArtificalCRL, CurrencyRewardLayer) {
-    // struct Fields {
-    //     bool visualOnly = false;
-    // };
+// i just wanted to shorten this constructor lol
+CurrencyRewardLayer* createArtificalCRL(int orbs, int diamonds, CCPoint position, CurrencyRewardType rewardType, float time) {
+    return CurrencyRewardLayer::create(orbs, 0, 0, diamonds, CurrencySpriteType::Icon, 0, CurrencySpriteType::Icon, 0, position, rewardType, 0, time);
+}
 
-    bool init(int orbs, int stars, int moons, int diamonds, CurrencySpriteType demonKey, 
-                int keyCount, CurrencySpriteType shardType, int shardsCount, CCPoint position, 
-                CurrencyRewardType rewardType, float yoffset, float time) {
-        if (!CurrencyRewardLayer::init(orbs, stars, moons, diamonds, demonKey, keyCount, shardType, shardsCount, position, rewardType, yoffset, time)) return false;
-        // geode::log::debug("{}", position);
-        return true;
-    }
-
-    // i just wanted to shorten this constructor lol
-    static ArtificalCRL* createArtifical(int orbs, int diamonds, CCPoint position, CurrencyRewardType rewardType, float time) {
-        ArtificalCRL* temp = reinterpret_cast<ArtificalCRL*>(CurrencyRewardLayer::create(orbs, 0, 0, diamonds, CurrencySpriteType::Icon, 0, CurrencySpriteType::Icon, 0, position, rewardType, 0, time));
-        return temp;
-    }
-};
-
-class $modify(bestFinder, PlayLayer) {
+class $modify(VisualFixPL, PlayLayer) {
     struct Fields {
         int currentBest = 0;
+        bool obtainedAllOrbs = false;
+        bool isSpecial = false;
     };
 
     bool init(GJGameLevel* p0, bool p1, bool p2) {
         if (!PlayLayer::init(p0,p1,p2)) return false;
         if (!m_isPlatformer) m_fields->currentBest = p0->getNormalPercent();
+
+        // early returned if unrated
+        int dif = m_level->m_stars.value();
+        if (dif <= 0) return true;
+
+        GameStatsManager* gsm = GameStatsManager::sharedState();
+        const int levelOrbs = gsm->getAwardedCurrencyForLevel(p0);
+        if (levelOrbs != orbCalc(100, dif)) {
+            m_fields->obtainedAllOrbs = true;
+            if (m_level->m_dailyID != 0 || m_level->m_gauntletLevel) {
+                m_fields->isSpecial = true;
+            }
+        } else {
+            m_fields->obtainedAllOrbs = false;
+             m_fields->isSpecial = false;
+
+        }
+
         return true;
     }
 
@@ -70,7 +73,7 @@ class $modify(bestFinder, PlayLayer) {
 
             // calculate collected diamonds restricting to just levels that have diamonds
             int diaInput = 0;
-            if ((m_level->m_dailyID != 0 || m_level->m_gauntletLevel)) {
+            if (m_fields->isSpecial) {
                 int collectedDia = gsm->getAwardedDiamondsForLevel(m_level);
                 int diaNow = diamondsCalc(getCurrentPercentInt(), dif);
                 if (diaNow - collectedDia < 0) {
@@ -82,7 +85,7 @@ class $modify(bestFinder, PlayLayer) {
             CCSize screenSize = CCDirector::sharedDirector()->getWinSize();
             CCPoint pos = {screenSize.width/2.f, screenSize.height/2.f};
 
-            ArtificalCRL* ACRL = ArtificalCRL::createArtifical(orbInput, diaInput, pos, CurrencyRewardType::Default, .9);
+            CurrencyRewardLayer* ACRL = createArtificalCRL(orbInput, diaInput, pos, CurrencyRewardType::Default, .9);
             ACRL->setZOrder(99);
             ACRL->setID("Artifical_CurrencyRewardLayer"_spr);
             this->addChild(ACRL);
@@ -94,5 +97,52 @@ class $modify(bestFinder, PlayLayer) {
             // reset player's best
             m_fields->currentBest = getCurrentPercentInt();
         }
+    }
+
+    /*
+    Extra methods (getters + setters)
+    */
+
+    bool hasAllOrbs() {
+        return m_fields->obtainedAllOrbs;
+    }
+
+    int currentBest() {
+        return m_fields->currentBest;
+    }
+
+    bool isSpecial() {
+        return m_fields->isSpecial;
+    }
+
+    void setCompleted() {
+        m_fields->currentBest = 100;
+    }
+};
+
+/*
+Artifical version of the CurrencyRewardLayer. Never directly adds anything
+*/
+class $modify(ArtificalCRL, CurrencyRewardLayer) {
+    bool init(int orbs, int stars, int moons, int diamonds, CurrencySpriteType demonKey, 
+                int keyCount, CurrencySpriteType shardType, int shardsCount, CCPoint position, 
+                CurrencyRewardType rewardType, float yoffset, float time) {
+        // checks if the level is rated and if the level has been completed before. Then alters visual case.
+        if (stars > 0 || moons > 0) {
+            if (VisualFixPL* VPL = reinterpret_cast<VisualFixPL*>(PlayLayer::get())) {
+                if (VPL->hasAllOrbs()) {
+                    int dif = std::max(stars, moons);
+                    int prevBest = VPL->currentBest();
+                    orbs += orbCalc(100, dif) - orbCalc(prevBest, dif);
+                    if (VPL->isSpecial()) {
+                        diamonds += diamondsCalc(100, dif) - diamondsCalc(prevBest, dif);
+                    }
+
+                    VPL->setCompleted();
+                }
+            }
+        }
+        // runs default function with possible added options
+        return CurrencyRewardLayer::init(orbs, stars, moons, diamonds, demonKey, keyCount, shardType, shardsCount, position, rewardType, yoffset, time);
     }
 };
